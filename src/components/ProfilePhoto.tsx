096156fd8969dabab028dd4764ea0f9f59d21338
Ryan Chikhi <118189321+Zeikrom251@users.noreply.github.com>
2025-01-15 23:47:53 +0100

    Update ProfilePhoto.tsx

--- src/components/ProfilePhoto.tsx
+++ src/components/ProfilePhoto.tsx
@@ -4,7 +4,7 @@ import "./ProfilePhoto.style.css";
 const ProfilePhoto: React.FC = () => {
   return (
     <div className="profile-photo">
-      <img src="../../public/profilPicture.jpg" alt="Photo de Profil" />
+      <img src="../../public/profil.jpg" alt="Photo de Profil" />
     </div>
   );
 };
